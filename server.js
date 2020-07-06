d7a7c051c4b56735355fcc27619da3e17ee41c76
Paras Agrawal <paras@piktorlabs.com>
2020-07-07 00:26:44 +0530

    refactor: Code change

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const debug = require("debug");
 
 // Models
 const UserModel = require("./models/User");
@@ -7,20 +8,30 @@ const UserModel = require("./models/User");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected");
+    debug("Mongoose: Successfully connected to the database");
+  })
+  .catch((err) => {
+    debug("Mongoose: Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
 
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose is connected!!!!");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("Mongoose is connected!!!!");
+// });
 
 app.get("/", (req, res) => {
   UserModel.find().then((response) => {
+    console.log("response", response);
     res.send(response);
   });
 });
