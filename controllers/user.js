aee5660a8c7173efa5d18209f363829806306c3a
Paras Agrawal <paras@piktorlabs.com>
2020-07-07 00:57:06 +0530

    refactor: Add consoles

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,10 @@
 const UserModel = require("../models/User");
 
 const getUsers = (req, res) => {
+  console.log("getUsers called");
   UserModel.find()
     .then((response) => {
+      console.log("users", response);
       res.send(response);
     })
     .catch((err) => {
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const userController = require("../controllers/user");
 
+console.log("inside routes");
 router.get("/", userController.getUsers);
 
 module.exports = router;
