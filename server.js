36bdf087def5f938093c9735507aa8ae2c9b3f3b
Paras Agrawal <paras@piktorlabs.com>
2020-07-07 19:08:23 +0530

    refactor: Small Change

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const debug = require("debug");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 const UserRoute = require("./routes/user");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
 console.log("MONGODB_URI", process.env.MONGODB_URI);
 console.log("PORT", process.env.PORT);
