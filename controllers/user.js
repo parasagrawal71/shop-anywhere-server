5240220a33a16e03431dd13d48901f4d44bed09d
Paras Agrawal <paras@piktorlabs.com>
2020-07-07 21:36:08 +0530

    refactor: DB connection in server.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,37 +1,38 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const UserModel = require("../models/User");
 
 const getUsers = (req, res) => {
   console.log("getUsers called");
   console.log("MONGODB_URI", process.env.MONGODB_URI);
 
-  mongoose
-    .connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
-    .then(() => {
-      console.log("connected");
-      UserModel.findOne({ name: "Paras" })
-        .then((response) => {
-          console.log("users", response);
-          res.status(200).send({
-            success: true,
-            response,
-          });
-        })
-        .catch((err) => {
-          res.send(err);
-        });
+  // mongoose
+  //   .connect(
+  //     process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
+  //     {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //     }
+  //   )
+  //   .then(() => {
+  //     console.log("connected");
+
+  //   })
+  //   .catch((err) => {
+  //     console.log(
+  //       "Mongoose: Could not connect to the database. Exiting now...",
+  //       err
+  //     );
+  //   });
+  UserModel.find()
+    .then((response) => {
+      // console.log("users", response);
+      res.status(200).send({
+        success: true,
+        response,
+      });
     })
     .catch((err) => {
-      console.log(
-        "Mongoose: Could not connect to the database. Exiting now...",
-        err
-      );
+      res.send(err);
     });
 };
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const debug = require("debug");
 // const bodyParser = require("body-parser");
 // const cors = require("cors");
@@ -15,27 +15,27 @@ const PORT = process.env.PORT || 5000;
 console.log("MONGODB_URI", process.env.MONGODB_URI);
 console.log("PORT", process.env.PORT);
 
-// mongoose
-//   .connect(
-//     process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("Mongoose: Successfully connected to the database");
-//   })
-//   .catch((err) => {
-//     console.log(
-//       "Mongoose: Could not connect to the database. Exiting now...",
-//       err
-//     );
-//   });
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/shop-anywhere",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Mongoose: Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log(
+      "Mongoose: Could not connect to the database. Exiting now...",
+      err
+    );
+  });
 
-// mongoose.connection.on("connected", () => {
-//   console.log("Mongoose is connected!!!!");
-// });
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!");
+});
 
 app.use("/api/users", UserRoute);
 
