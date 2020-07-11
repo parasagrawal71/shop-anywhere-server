2b591522894c6a6c83f4ec87c20cb9342d1419a8
Paras Agrawal <parasagrawal71@gmail.com>
2020-07-12 00:49:05 +0530

    feat: Add body-parser to read request's body

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const apiRouter = require("./src/api/api.router");
 require("./src/utils/db.connect");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use("/api/v1", apiRouter);
 
 app.get("/", (req, res) => {
