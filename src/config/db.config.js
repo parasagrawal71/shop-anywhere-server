6a1fcf0894c7ada44361fad44854370d6a4504e7
Paras Agrawal <parasagrawal71@gmail.com>
2020-07-20 01:30:27 +0530

    user password updated

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB_URI:
-    "mongodb+srv://parasagrawal71:Justdoit@321@shop-anywhere-server-cl.2eenk.mongodb.net/shop-anywhere?retryWrites=true&w=majority",
+    "mongodb+srv://parasagrawal71:user-password-123@shop-anywhere-server-cl.2eenk.mongodb.net/shop-anywhere?retryWrites=true&w=majority",
   LOCAL_MONGODB: "mongodb://localhost:27017/shop-anywhere",
 };
