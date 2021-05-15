5d9805a523e01c1d614cdd4e05ed1af118b7390d
Paras Agrawal <parasagrawal71@gmail.com>
2021-05-15 19:01:40 +0530

    Update db.connect.js

--- src/utils/db.connect.js
+++ src/utils/db.connect.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
-const { MONGODB_URI } = require("../config/db.config");
+const { LOCAL_MONGODB } = require("../config/db.config");
 
-const connectDatabase = mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch((e) => {
-    console.log("Could not connect to the database. Exiting now...", e);
-    process.exit();
-  });
+const connectDatabase = () => {
+  let databaseUrl = null;
+  if (process.env.NODE_ENV === "production") {
+    databaseUrl = process.env.MONGODB_URI;
+  } else {
+    databaseUrl = LOCAL_MONGODB;
+  }
+
+  return mongoose
+    .connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to the database");
+    })
+    .catch((e) => {
+      console.log("Could not connect to the database. Exiting now...", e);
+      process.exit();
+    });
+};
+
+connectDatabase();
 
 mongoose.connection.on("connected", () => {
   console.log("Database is connected!!!!");
 });
-
-module.exports = connectDatabase;
